cb682d973b191809c4a940ab1771be5ff35bedef
Patrick McCabe <patrickmccb0@gmail.com>
2013-09-01 15:34:08 -0400

    Added initialize method to .h file and an instance variable for the led strip
    I also had to remove the #include of the Adafruit_Ws2801.h file to get
    the .cpp to compile without error. The instance variable LEDStrip was
    added because it will be assigned an object of type Adafruit_WS2801.

--- MIT_Lighted_Sign/MITLight.cpp
+++ MIT_Lighted_Sign/MITLight.cpp
@@ -2,9 +2,10 @@
 #include "Arduino.h"
 #include "MITLight.h"
 #include "SPI.h"
-#include "Adafruit_WS2801.h"
 
 MITLight:: MITLight(int dataPin, int clockPin){
   _dataPin = dataPin;
   _clockPin = clockPin;
 }
+
+
--- MIT_Lighted_Sign/MITLight.h
+++ MIT_Lighted_Sign/MITLight.h
@@ -3,17 +3,18 @@
 #define MITLight_h
 
 #include "Arduino.h"
+#include "Adafruit_WS2801.h"
 
 class MITLight{
   
   public:
     MITLight(int dataPin, int clockPin);
+    void initialize();
     
   private:
     int _dataPin;
     int _clockPin;
-    
- 
+    Adafruit_WS2801 _LEDStrip; 
 };
 
 
