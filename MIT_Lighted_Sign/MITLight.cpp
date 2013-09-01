25092c15b06af8c2fbd8bf95138414ed41408041
Patrick McCabe <patrickmccb0@gmail.com>
2013-09-01 15:38:13 -0400

    Implemented the initialize method in the .cpp file

--- MIT_Lighted_Sign/MITLight.cpp
+++ MIT_Lighted_Sign/MITLight.cpp
@@ -9,3 +9,8 @@ MITLight:: MITLight(int dataPin, int clockPin){
 }
 
 
+void MITLight:: initialize(){
+  _LEDStrip = Adafruit_WS2801(25, _dataPin, _clockPin); // creates the LED strip object
+  _LEDStrip.begin();                                    // initialize the LED strip object
+  _LEDStrip.show();                     // Update LED contents, to start they are all 'off'
+}
